49b891ce26cc03545ba45743ade046a1757b7fa7
Gabriel Cabral <gabrielcabral@MacBook-Pro-de-Gabriel.local>
2025-05-10 22:08:46 -0300

    feat: primeiro commit

---
+++ src/main/kotlin/gabriel/dev/spring_boot_crud_kotlin/SpringBootCrudKotlinApplication.kt
@@ -0,0 +1,11 @@
+package gabriel.dev.spring_boot_crud_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootCrudKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootCrudKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/gabriel/dev/spring_boot_crud_kotlin/SpringBootCrudKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package gabriel.dev.spring_boot_crud_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootCrudKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
